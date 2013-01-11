f15017127afb4701750a3502cb6c8324334a529c
Mike <mike.capozzoli@gmail.com>
2013-01-10 22:22:51 -0500

    new approach to server

--- Mike/SWIMR/EthernetCommunication/swim_server.py
+++ Mike/SWIMR/EthernetCommunication/swim_server.py
@@ -1,34 +1,19 @@
-# Echo server program
-import SocketServer
-import sys
+import socket
 
-class MyUDPHandler(SocketServer.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        print data
-        socket.sendto(data.upper(), self.client_address)
-        self.DATA = data
-    
-        
-        
+# A UDP server
 
-if __name__ == "__main__":
-    HOST = sys.argv[1]
-    PORT = int(sys.argv[2])
-    if HOST is None or PORT is None:
-        print "try again stupid: python swim_server.py 153.106.75.171 9999"
-        exit(1)
-    # HOST, PORT = "153.106.113.107", 9999
-    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
-    #server.serve_forever()
-    server.handle_request()
-    print server.RequestHandlerClass.DATA
-    
+# Set up a UDP server
+UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# Listen on port 21567
+# (to all IP addresses on this system)
+listen_addr = ("",9999)
+UDPSock.bind(listen_addr)
+
+# Report on all data packets received and
+# where they came from in each case (as this is
+# UDP, each may be from a different source and it's
+# up to the server to sort this out!)
+while True:
+        data,addr = UDPSock.recvfrom(1024)
+        print data.strip(),addr
