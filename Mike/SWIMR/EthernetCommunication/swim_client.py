3e7dac86b51c07917621a2dc86ed3060396db780
Mike <mike.capozzoli@gmail.com>
2013-01-13 23:39:35 -0500

    made a bunch of improvements

--- Mike/SWIMR/EthernetCommunication/swim_client.py
+++ Mike/SWIMR/EthernetCommunication/swim_client.py
@@ -4,6 +4,7 @@ Created on Oct 31, 2012
 @author: Mike
 '''
 import socket
+from socket import error 
 #import sys
 #data = "test".join(sys.argv[1:])
 
@@ -30,47 +31,54 @@ class SwimClient(object):
         self.PAYLOAD = "default"
         self.RECEIVE = ''
         self.ISCONNECTED = False
+        self.MAXPACKETSIZE = 32
         self.HOSTPORT = (self.HOST, self.PORT)
         self.initialize()
+        
 
     def initialize(self):
         '''
-        initializes 
+        initializes connection to server
         '''
         # SOCK_DGRAM is the socket type to use for UDP sockets
         # AF_INET sets it to use UDP protocol
         self.SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # sets socket to be nonblocking, if data can;t immediately be sent or received then an exception is raised
+        self.SOCK.setblocking(0)
         
         #Find the server
         self.setpayload("Hello!")
         while 1:
             self.SOCK.sendto(self.PAYLOAD,self.HOSTPORT)
-
-            data, addr = self.SOCK.recvfrom(1024)
-            print data
-            print data.strip()
-            if data.strip() == 'hello client':
-                print"I've found the server"
-                self.ISCONNECTED == True
-                break
-        
-        
+            
+            try:
+                data, addr = self.SOCK.recvfrom(1024)
+                print data, addr
+                if data.strip() == 'hello client':
+                    print"I've found the server"
+                    self.ISCONNECTED = True
+                    break 
+            except error:
+                continue        
     def send(self):
-        if len(self.PAYLOAD)<=8192 and len(self.PAYLOAD)>0:
+        if len(self.PAYLOAD)<=self.MAXPACKETSIZE and len(self.PAYLOAD)>0:
             self.SOCK.sendto(self.PAYLOAD,self.HOSTPORT)
-            self.SOCK.sendto('Done',(self.HOST,self.PORT))
+            self.SOCK.sendto('done',(self.HOST,self.PORT))
         else:
-            self.SOCK.sendto(self.PAYLOAD[:8192], self.HOSTPORT)
-            self.helpersend(self.PAYLOAD[8192:])
+            self.SOCK.sendto(self.PAYLOAD[:self.MAXPACKETSIZE], self.HOSTPORT)
+            self.helpersend(self.PAYLOAD[self.MAXPACKETSIZE:])
             
             #self.SOCK.sendto(self.PAYLOAD + "\n", (self.HOST, self.PORT))
     def helpersend(self,payload):
-        if len(payload)<=8192 and len(payload)>0:
+        '''
+        don't call helpersend directly.  
+        '''
+        if len(payload)<=self.MAXPACKETSIZE and len(payload)>0:
             self.SOCK.sendto(payload,self.HOSTPORT)
-            self.SOCK.sendto(payload, self.HOSTPORT)
+            self.SOCK.sendto('done', self.HOSTPORT)
         else:
-            self.SOCK.sendto(payload[:8192], self.HOSTPORT)
-            self.helpersend(payload[8192:])
+            self.SOCK.sendto(payload[:self.MAXPACKETSIZE], self.HOSTPORT)
+            self.helpersend(payload[self.MAXPACKETSIZE:])
         
     def setpayload(self, payload):
         self.PAYLOAD = payload
@@ -80,13 +88,16 @@ class SwimClient(object):
     
     def receive(self, size = int()):
         self.RECEIVE = ''
-        receivedstring = self.SOCK.recv(size)
-        while receivedstring != "done":
-            self.RECEIVE = self.RECEIVE + receivedstring
-            receivedstring = self.SOCK.recv(size)
+        receivedstring = str()
+        while 1:
+            try:
+                receivedstring = self.SOCK.recv(size)
+            except error:
+                continue
             if receivedstring == 'done':
                 break
-        
+            else:
+                self.RECEIVE = self.RECEIVE + receivedstring  
 
 if __name__=='__main__':
     #from swim_client import SwimClient
@@ -95,13 +106,14 @@ if __name__=='__main__':
     try:
         IP,PORT = str(sys.argv[1]),int(sys.argv[2])
     except:
-        print" you forgot command line arguments stupid"
+        print "try: python swim_client.py <serverIP> <port>"
         exit(1)
     c = SwimClient(IP,PORT)
 
     while 1:
-        shit = raw_input("what?: ")
-        c.setpayload(shit)
+        #stuff = raw_input("what?: ")
+        this = raw_input("what?: ")
+        c.setpayload(this)
         c.send()
         c.receive(1024)
         print "RPi says: " + c.getreceive()
