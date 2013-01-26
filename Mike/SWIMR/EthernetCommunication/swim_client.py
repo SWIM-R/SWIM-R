c39345cc9aa543bf7b6d6ee897a9741244ee0a01
Mike <mike.capozzoli@gmail.com>
2013-01-25 22:32:21 -0500

    commit all the things

--- Mike/SWIMR/EthernetCommunication/swim_client.py
+++ Mike/SWIMR/EthernetCommunication/swim_client.py
@@ -17,7 +17,7 @@ class SwimClient(threading.Thread):
     '''
     def __init__(self, host = str(), port = int() ):
         '''
-        Constructor
+        Initializes...everything.  
         '''
         threading.Thread.__init__(self)
         
@@ -39,9 +39,11 @@ class SwimClient(threading.Thread):
         self.initialize()
         self.stopreceivethread = False
         self.daemon = True
+        
+        
     def initialize(self):
         '''
-        initializes connection to server
+        initializes connection to server, if successfully initialized sets ISCONNECTED to be true
         '''
         # SOCK_DGRAM is the socket type to use for UDP sockets
         # AF_INET sets it to use UDP protocol
@@ -69,6 +71,9 @@ class SwimClient(threading.Thread):
             except error:
                 continue        
     def send(self):
+        '''
+        sends whatever is in self.PAYLOAD. calls helpersend if it is large message.  Sends 'done' at the end of a packet
+        '''
         if len(self.PAYLOAD)<=self.MAXPACKETSIZE and len(self.PAYLOAD)>0:
             self.SOCK.sendto(self.PAYLOAD,self.HOSTPORT)
             self.SOCK.sendto('done',(self.HOST,self.PORT))
@@ -77,7 +82,7 @@ class SwimClient(threading.Thread):
             self.helpersend(self.PAYLOAD[self.MAXPACKETSIZE:])            
     def helpersend(self,payload):
         '''
-        don't call helpersend directly.  
+        don't call helpersend directly.  sends 'done' at the end of a packet
         '''
         if len(payload)<=self.MAXPACKETSIZE and len(payload)>0:
             self.SOCK.sendto(payload,self.HOSTPORT)
@@ -87,6 +92,9 @@ class SwimClient(threading.Thread):
             self.helpersend(payload[self.MAXPACKETSIZE:])
             
     def isconnected(self):
+        '''
+        This doesn't work yet.  Its tricky because everything is UDP...and is never really connected in the first place
+        '''
         self.SOCK.sendto("you there?",self.HOSTPORT)
         self.SOCK.setblocking(1)
         self.SOCK.settimeout(5.0)
@@ -99,12 +107,22 @@ class SwimClient(threading.Thread):
             return False
         
     def setpayload(self, payload):
+        '''
+        setter for the payload that is going to be sent 
+        '''
         self.PAYLOAD = payload
         
     def getreceive(self):
+        '''
+        getter for whatever has been received
+        '''
         return self.RECEIVE
     
     def receive(self, size = int()):
+        '''
+        receives a packet until it gets 'done'.  the packet is stored in RECEIVE
+        '''
+        
         self.RECEIVE = ''
         receivedstring = str()
         while 1:
@@ -117,6 +135,10 @@ class SwimClient(threading.Thread):
             else:
                 self.RECEIVE = self.RECEIVE + receivedstring  
     def run(self):
+        '''
+        implementation of the inherited run() method from the Thread class.  
+        This is a separate thread from the main thread that is always receiving information
+        '''
         while c.stopreceivethread == False:
             self.receive(self.MAXPACKETSIZE)
             print "RPI says: " + self.RECEIVE
@@ -141,17 +163,32 @@ if __name__=='__main__':
             c.start()
             ############
 
+
+
             #loop()
             while c.ISCONNECTED:
                 c.setpayload(raw_input("What: "))
                 c.send()
                 
-               ###Connection Checking doesn't work yet 
-               # c.ISCONNECTED = c.isconnected()
+                ###Connection Checking doesn't work yet 
+                # c.ISCONNECTED = c.isconnected()
                 print c.ISCONNECTED
-            ##########   
+            ##########  
+            
+            
+            #cleanup() 
+            #Things in this section are called if something goes wrong in loop
             c.stopreceivethread = True  
             c.SOCK.close()  
+            ######
+            
+            
+            
+            
+            
+            
+            
+            
         except KeyboardInterrupt:
             print "bye bye"
             exit(0)
