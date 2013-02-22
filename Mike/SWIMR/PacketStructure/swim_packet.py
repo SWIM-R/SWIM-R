d7fa133e6c12eac6a72d2a585b8b233e5fbc20d1
Mike <mike.capozzoli@gmail.com>
2013-02-22 16:20:29 -0500

    major improvements to client_interface and swim_packet

--- Mike/SWIMR/PacketStructure/swim_packet.py
+++ Mike/SWIMR/PacketStructure/swim_packet.py
@@ -3,59 +3,25 @@ Created on Jan 25, 2013
 
 @author: Mike
 '''
-import ast
 class SwimPacket():
     '''
     classdocs
     '''
 
 
-    def __init__(self,DICT = dict()):
-        '''
-        Edit the member values of this class directly, please.  I don't think we need getters and setters.....
+    def __init__(self):
+      
+        self.YAW = 0
+        self.PITCH = 0
+        self.ROLL = 0 
+        self.X = 0 
+        self.Y = 0
+        self.Z = 0
         
-        If you initialize it with a string that was just received:
-            2. each key of the dictionary becomes a member of the instance of SwimPacket
-            3. each member is initialized with the value the key points to
-        '''
-        
-        #if not initialized with anything, ititialize some garbage values
-        if DICT is None:
-            self.header = "hello"
-            self.format_string = "how"
-            self.image_string = "are"
-            self.command = "you"
-        #otherwise initialize all members with the string
-        else:
-            for key in DICT:
-                setattr(self,key,DICT[key])
-    
-        
-
-    
-    def __str__(self):
-        '''
-        LOOK AT HOW COOL PYTHON IS
-        compresses all of the class attributes into a string to be loaded into .setpayload()
-        returns the string that is about to be sent.  You need to call this if you update the members
-        '''
-        return str(self.__dict__)
 
 
 if __name__=='__main__':
     packet = SwimPacket()
-    
-    t = packet.sealpacket()
-    
-    print t
-    
-    #Converting from a string to a dictionary
-    dictionary = t.__dict__
-    
-    dictionary['new'] = 'thing'
-    STRING =  str(dictionary)
-    d = SwimPacket(STRING)
-    
-    print vars(d)
-    
+    dictionary = packet.__dict__
+    print dictionary
 
--- Mike/SWIMR/client_interface.py
+++ Mike/SWIMR/client_interface.py
@@ -25,10 +25,12 @@ class ClientInterface(threading.Thread):
     
     def __init__(self, host = str(), port = int()):
         threading.Thread.__init__(self)
+        self.daemon = True
+        self.packet = SwimPacket()
         if host == "":
             self.IP = "153.106.75.171"
         else:
-            self.HOST = host
+            self.IP = host
         
         if port is None:
             self.PORT = 9999
@@ -40,45 +42,91 @@ class ClientInterface(threading.Thread):
                         
     def run(self):
         while 1:
-            try:
-                ########setup()#########
-                
-                #Setting up Ethernet Communication
-                print 'finding server....'
-                ethernet = SwimClient(self.IP,self.PORT)
-                print 'server found......'
-                print 'starting receive thread'
-                ethernet.start()
-                ############
-                #########################
-                
-                
-                ############loop()#######
-                #main loop of the program
-                while ethernet.ISCONNECTED:
-                    time.sleep(0.2)
-                    ethernet.setpayload(self.PAYLOAD)
-                    ethernet.send()
-                    if ethernet.ISCONNECTED:
-                        print "still connected"
-                    else:
-                        print 'not connected!'
-                        
-                    if ethernet.NEWMESSAGE:
-                        print "this is a new message: " + ethernet.getreceive()
-                    else:
-                        print 'this is not a new message: ' + ethernet.getreceive()
-                ######################### 
-                 
-                 
-                 
-                ###########cleanup()#####
-                #Things in this section are called if something goes wrong in loop()
-                print "cleaning up"
-                ethernet.cleanup()
-                print 'cleaned up!'
-                ########################
-            except KeyboardInterrupt:
-                print "bye bye"
-                exit(0)
+            ########setup()#########
+            
+            #Setting up Ethernet Communication
+            print 'finding server....'
+            ethernet = SwimClient(self.IP,self.PORT)
+            print 'server found......'
+            print 'starting receive thread'
+            ethernet.start()
+            ############
+            #########################
+            
+            
+            ############loop()#######
+            #main loop of the program
+            while ethernet.ISCONNECTED:
+                time.sleep(0.2)
+                self.updatepayload()
+                ethernet.setpayload(self.PAYLOAD)
+                ethernet.send()
+                if ethernet.ISCONNECTED:
+                    print "still connected"
+                else:
+                    print 'not connected!'
+                    break
+                    
+                if ethernet.NEWMESSAGE:
+                    print "this is a new message: " + ethernet.getreceive()
+                else:
+                    print 'this is not a new message: ' + ethernet.getreceive()
+                    
+            ######################### 
+             
+             
+             
+            ###########cleanup()#####
+            #Things in this section are called if something goes wrong in loop()
+            print "cleaning up"
+            ethernet.cleanup()
+            print 'cleaned up!'
+            ########################
+    
+    
+    def updatepayload(self):
+        self.PAYLOAD = str(self.packet.__dict__)
         
+    def setX(self,x = int()):
+        self.packet.X = x
+    
+    def setY(self,y = int()):
+        self.packet.Y = y
+    
+    def setZ(self,z = int()):
+        self.packet.Z = z
+        
+    def setYaw(self,yaw = int()):
+        self.packet.YAW = yaw
+        
+    def setPitch(self, pitch = int()):
+        self.packet.PITCH = pitch
+    
+    def setRoll(self,roll = int()):
+        self.packet.ROLL = roll
+        
+    
+    
+if __name__ == '__main__':
+    
+    try:
+        try:
+            IP,PORT = str(sys.argv[1]),int(sys.argv[2])
+        except:
+            print "try: python clientmain.py <SERVERIP> <PORT>"
+            exit(1)
+            
+        clientinterface = ClientInterface(IP,PORT)
+        clientinterface.run()
+        
+    except KeyboardInterrupt:
+        print "bye bye"
+        exit(0)
+    
+    
+    
+    
+    
+    
+    
+    
