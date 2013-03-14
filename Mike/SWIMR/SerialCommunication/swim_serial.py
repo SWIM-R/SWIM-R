4dc891293d4ffb2116dc094b30c9088834a9e852
Mike <mike.capozzoli@gmail.com>
2013-03-14 18:31:37 -0400

    serial debugging

--- Mike/SWIMR/SerialCommunication/swim_serial.py
+++ Mike/SWIMR/SerialCommunication/swim_serial.py
@@ -114,6 +114,7 @@ class SwimSerial(threading.Thread):
                         continue
                     #__TODO__ 
                 if self.SERIAL is not None:
+                    time.sleep(3.0) # wait for the arduino to be ready
                     self.SERIAL.flushInput()
                     self.SERIAL.flushOutput()
                     self.ISCONNECTED = True
