13870a6138491a20983426d7e029111b3d1bf39b
root <root@raspberrypi.(none)>
2013-04-30 03:58:25 +0000

    Revert "stash"
    This reverts commit 1e280ba861c46ef62d87cea0e246e5336f5a48a4.

--- Mike/SWIMR/SerialCommunication/swim_serial.py
+++ Mike/SWIMR/SerialCommunication/swim_serial.py
@@ -178,7 +178,6 @@ class SwimSerial(threading.Thread):
             try:
                 for number in self.PAYLOAD:
                     self.SERIAL.write(unichr(number).encode('latin_1')) #So that 0-255 can be encoded into a byte
-		    print unichr(number).encode('latin_1')
             except: # timeout
                 self.ISCONNECTED = False
 
